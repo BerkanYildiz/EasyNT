7248246a5eff6cf73c124c890996d5de89d205e8
BerkanYildiz <BerkanYildiz@users.noreply.github.com>
2022-12-17 19:59:40 +0100

    Win11 workaround

--- src/Sources/Extensions/MemoryExtensions.cpp
+++ src/Sources/Extensions/MemoryExtensions.cpp
@@ -184,6 +184,336 @@ NTSTATUS CkZeroVirtualMemory(CONST PEPROCESS InProcess, CONST PVOID InBaseAddres
 	return STATUS_SUCCESS;
 }
 
+#define _WIN11_WORKAROUND
+#ifdef _WIN11_WORKAROUND
+//
+// The maximum amount to try to Probe and Lock is 14 pages, this
+// way it always fits in a 16 page allocation.
+//
+
+#define MAX_LOCK_SIZE ((ULONG)(14 * PAGE_SIZE))
+
+//
+// The maximum to move in a single block is 64k bytes.
+//
+
+#define MAX_MOVE_SIZE (LONG)0x10000
+
+//
+// Define the pool move threshold value.
+//
+
+#define POOL_MOVE_THRESHOLD 511
+
+#define COPY_STACK_SIZE 64
+
+NTSTATUS
+MiDoPoolCopy(
+	IN PEPROCESS FromProcess,
+	IN CONST VOID* FromAddress,
+	IN PEPROCESS ToProcess,
+	OUT PVOID ToAddress,
+	IN SIZE_T BufferSize,
+	IN KPROCESSOR_MODE PreviousMode,
+	OUT PSIZE_T NumberOfBytesRead
+)
+
+/*++
+Routine Description:
+	This function copies the specified address range from the specified
+	process into the specified address range of the current process.
+Arguments:
+	 ProcessHandle - Supplies an open handle to a process object.
+	 BaseAddress - Supplies the base address in the specified process
+				   to be read.
+	 Buffer - Supplies the address of a buffer which receives the
+			  contents from the specified process address space.
+	 BufferSize - Supplies the requested number of bytes to read from
+				  the specified process.
+	 PreviousMode - Supplies the previous processor mode.
+	 NumberOfBytesRead - Receives the actual number of bytes
+						 transferred into the specified buffer.
+Return Value:
+	NTSTATUS.
+--*/
+
+{
+	KAPC_STATE ApcState;
+	SIZE_T AmountToMove;
+	LOGICAL ExceptionAddressConfirmed;
+	PEPROCESS CurrentProcess;
+	CONST VOID* InVa;
+	SIZE_T LeftToMove;
+	SIZE_T MaximumMoved;
+	PVOID OutVa;
+	PVOID PoolArea;
+	LONGLONG StackArray[COPY_STACK_SIZE];
+	ULONG FreePool;
+	SIZE_T PageOffset;
+
+	PAGED_CODE();
+
+	ASSERT(BufferSize != 0);
+
+	//
+	// Get the address of the current process object and initialize copy
+	// parameters.
+	//
+
+	CurrentProcess = PsGetCurrentProcess();
+
+	InVa = FromAddress;
+	OutVa = ToAddress;
+
+	//
+	// Allocate non-paged memory to copy in and out of.
+	//
+
+	MaximumMoved = MAX_MOVE_SIZE;
+	if (BufferSize <= MAX_MOVE_SIZE)
+	{
+		MaximumMoved = BufferSize;
+	}
+
+	FreePool = FALSE;
+	if (BufferSize <= sizeof(StackArray))
+	{
+		PoolArea = (PVOID)&StackArray[0];
+	}
+	else
+	{
+		do
+		{
+			PoolArea = ExAllocatePoolWithTag(NonPagedPool, MaximumMoved, 'wRmM');
+			if (PoolArea != NULL)
+			{
+				FreePool = TRUE;
+				break;
+			}
+
+			MaximumMoved = MaximumMoved >> 1;
+			if (MaximumMoved <= sizeof(StackArray))
+			{
+				PoolArea = (PVOID)&StackArray[0];
+				break;
+			}
+		} while (TRUE);
+	}
+
+	//
+	// Initializing BadVa & ExceptionAddressConfirmed is not needed for
+	// correctness but without it the compiler cannot compile this code
+	// W4 to check for use of uninitialized variables.
+	//
+
+	ExceptionAddressConfirmed = FALSE;
+
+	//
+	// Copy the data into pool, then copy back into the ToProcess.
+	//
+
+	LeftToMove = BufferSize;
+	AmountToMove = MaximumMoved;
+
+	while (LeftToMove > 0)
+	{
+		if (LeftToMove < AmountToMove)
+		{
+			//
+			// Set to move the remaining bytes.
+			//
+
+			AmountToMove = LeftToMove;
+		}
+
+		KeStackAttachProcess(FromProcess, &ApcState);
+
+		//
+		// Probe to make sure that the specified buffer is accessible in
+		// the target process.
+		//
+
+		if ((InVa == FromAddress) && (PreviousMode != KernelMode))
+		{
+			// ProbeForRead(FromAddress, BufferSize, sizeof(CHAR));
+		}
+
+		for (PageOffset = 0; PageOffset < PAGE_ROUND_UP(AmountToMove); PageOffset += PAGE_SIZE)
+		{
+			if (!MmIsAddressValid(RtlAddOffsetToPointer(OutVa, PageOffset)))
+			{
+				ExceptionAddressConfirmed = TRUE;
+				goto BadAddress;
+			}
+		}
+
+		RtlCopyMemory(PoolArea, InVa, AmountToMove);
+
+		KeUnstackDetachProcess(&ApcState);
+
+		KeStackAttachProcess(ToProcess, &ApcState);
+
+		//
+		// Now operating in the context of the ToProcess.
+		//
+
+		if ((InVa == FromAddress) && (PreviousMode != KernelMode))
+		{
+			// ProbeForWrite(ToAddress, BufferSize, sizeof(CHAR));
+		}
+
+		for (PageOffset = 0; PageOffset < PAGE_ROUND_UP(AmountToMove); PageOffset += PAGE_SIZE)
+		{
+			if (!MmIsAddressValid(RtlAddOffsetToPointer(OutVa, PageOffset)))
+			{
+				ExceptionAddressConfirmed = TRUE;
+				goto BadAddress;
+			}
+		}
+
+		RtlCopyMemory(OutVa, PoolArea, AmountToMove);
+
+	BadAddress:
+		if (ExceptionAddressConfirmed)
+		{
+			//
+			// If an exception occurs during the move operation or probe,
+			// return the exception code as the status value.
+			//
+
+			KeUnstackDetachProcess(&ApcState);
+
+			if (FreePool)
+			{
+				ExFreePool(PoolArea);
+			}
+
+			//
+			// If the failure occurred during the move operation, determine
+			// which move failed, and calculate the number of bytes
+			// actually moved.
+			//
+
+			*NumberOfBytesRead = BufferSize - LeftToMove;
+
+			return STATUS_PARTIAL_COPY;
+		}
+
+		KeUnstackDetachProcess(&ApcState);
+
+		LeftToMove -= AmountToMove;
+		InVa = (PVOID)((ULONG_PTR)InVa + AmountToMove);
+		OutVa = (PVOID)((ULONG_PTR)OutVa + AmountToMove);
+	}
+
+	if (FreePool)
+	{
+		ExFreePool(PoolArea);
+	}
+
+	//
+	// Set number of bytes moved.
+	//
+
+	*NumberOfBytesRead = BufferSize;
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS
+MmCopyVirtualMemoryImpl(
+	IN PEPROCESS FromProcess,
+	IN CONST VOID* FromAddress,
+	IN PEPROCESS ToProcess,
+	OUT PVOID ToAddress,
+	IN SIZE_T BufferSize,
+	IN KPROCESSOR_MODE PreviousMode,
+	OUT PSIZE_T NumberOfBytesCopied
+)
+{
+	NTSTATUS Status;
+	PEPROCESS ProcessToLock;
+
+	if (BufferSize == 0)
+	{
+		ASSERT(FALSE);         // No one should call with a zero size.
+		return STATUS_SUCCESS;
+	}
+
+	ProcessToLock = FromProcess;
+	if (FromProcess == PsGetCurrentProcess())
+	{
+		ProcessToLock = ToProcess;
+	}
+
+	//
+	// Make sure the process still has an address space.
+	//
+
+	if (NT_ERROR(Status = PsAcquireProcessExitSynchronization(ProcessToLock)))
+	{
+		return Status;
+	}
+
+	//
+	// If the buffer size is greater than the pool move threshold,
+	// then attempt to write the memory via direct mapping.
+	//
+
+	/*if (BufferSize > POOL_MOVE_THRESHOLD)
+	{
+		Status = MiDoMappedCopy(FromProcess,
+								FromAddress,
+								ToProcess,
+								ToAddress,
+								BufferSize,
+								PreviousMode,
+								NumberOfBytesCopied);
+
+		//
+		// If the completion status is not a working quota problem,
+		// then finish the service. Otherwise, attempt to write the
+		// memory through nonpaged pool.
+		//
+
+		if (Status != STATUS_WORKING_SET_QUOTA)
+		{
+			goto CompleteService;
+		}
+
+		*NumberOfBytesCopied = 0;
+	}*/
+
+	//
+	// There was not enough working set quota to write the memory via
+	// direct mapping or the size of the write was below the pool move
+	// threshold. Attempt to write the specified memory through nonpaged
+	// pool.
+	//
+
+	Status = MiDoPoolCopy(FromProcess,
+		FromAddress,
+		ToProcess,
+		ToAddress,
+		BufferSize,
+		PreviousMode,
+		NumberOfBytesCopied);
+
+	//
+	// Dereference the target process.
+	//
+
+CompleteService:
+
+	//
+	// Indicate that the vm operation is complete.
+	//
+
+	PsReleaseProcessExitSynchronization(ProcessToLock);
+
+	return Status;
+}
+#endif
+
 /// <summary>
 /// Safely copy virtual memory from the source address to the destination address.
 /// </summary>
@@ -228,7 +558,11 @@ NTSTATUS CkCopyVirtualMemory(CONST PEPROCESS InSourceProcess, CONST PVOID InSour
 	// 
 
 	SIZE_T NumberOfBytesCopied = 0;
+#ifndef _WIN11_WORKAROUND
 	Status = MmCopyVirtualMemory(InSourceProcess, InSourceAddress, InDestinationProcess, InDestinationAddress, InNumberOfBytes, KernelMode, &NumberOfBytesCopied);
+#else
+	Status = MmCopyVirtualMemoryImpl(InSourceProcess, InSourceAddress, InDestinationProcess, InDestinationAddress, InNumberOfBytes, KernelMode, &NumberOfBytesCopied);
+#endif
 
 	// 
 	// Return the result.
