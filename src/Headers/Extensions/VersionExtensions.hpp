5d0d123d7f01430056c0ebcb9a42874b60b2bb06
BerkanYildiz <BerkanYildiz@users.noreply.github.com>
2024-01-14 19:04:01 +0100

    Platform version checking

--- src/Headers/Extensions/VersionExtensions.hpp
+++ src/Headers/Extensions/VersionExtensions.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#define OS_PLATFORM_WINDOWS_11 11
+#define OS_PLATFORM_WINDOWS_10 10
+#define OS_PLATFORM_WINDOWS_7 7
+
 /// <summary>
 /// Gets the current major version number for this operating system.
 /// </summary>
@@ -14,3 +18,8 @@ ULONG RtlGetVersionMinorNumber();
 /// Gets the current build version number for this operating system.
 /// </summary>
 ULONG RtlGetVersionBuildNumber();
+
+/// <summary>
+/// Gets a value indicating whether the current platform is the one passed as argument.
+/// </summary>
+BOOLEAN RtlCurrentPlatformIs(BYTE InPlatform);
--- src/Sources/Extensions/VersionExtensions.cpp
+++ src/Sources/Extensions/VersionExtensions.cpp
@@ -32,3 +32,36 @@ ULONG RtlGetVersionBuildNumber()
 	RtlGetVersion(&VersionInfo);
 	return VersionInfo.dwBuildNumber;
 }
+
+/// <summary>
+/// Gets a value indicating whether the current platform is the one passed as argument.
+/// </summary>
+BOOLEAN RtlCurrentPlatformIs(BYTE InPlatform)
+{
+	const auto CurrentBuildNumber = RtlGetVersionBuildNumber();
+
+	switch (InPlatform)
+	{
+		case OS_PLATFORM_WINDOWS_11:
+		{
+			return CurrentBuildNumber >= 22000 &&
+				   CurrentBuildNumber <= 22631;
+		}
+
+		case OS_PLATFORM_WINDOWS_10:
+		{
+			return CurrentBuildNumber >= 10240 &&
+				   CurrentBuildNumber <  22000;
+		}
+
+		case OS_PLATFORM_WINDOWS_7:
+		{
+			return CurrentBuildNumber == 7601;
+		}
+
+		default:
+		{
+			return false;
+		}
+	}
+}
